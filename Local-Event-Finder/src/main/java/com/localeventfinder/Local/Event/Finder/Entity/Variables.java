e89d6c008c90038c1465b1a834a487213e4f76fe
Parth Sitoot <parth.sitoot@gmail.com>
2025-02-15 17:58:54 +0530

    Adding user classes

--- Local-Event-Finder/src/main/java/com/localeventfinder/Local/Event/Finder/Entity/Variables.java
+++ Local-Event-Finder/src/main/java/com/localeventfinder/Local/Event/Finder/Entity/Variables.java
@@ -1,12 +1,14 @@
 package com.localeventfinder.Local.Event.Finder.Entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
-@Document
+@Document(collection = "Events")
+@Data
 public class Variables {
     @Id
     ObjectId id;
@@ -21,51 +23,4 @@ public class Variables {
 
     LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public String getVenue() {
-        return venue;
-    }
-
-    public void setVenue(String venue) {
-        this.venue = venue;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getDetails() {
-        return details;
-    }
-
-    public void setDetails(String details) {
-        this.details = details;
-    }
 }
